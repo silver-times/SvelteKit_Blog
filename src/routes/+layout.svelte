2a7a439f14b91af19aee8a5c6e3adc2230ab7ac0
Rajat Sharma <82762243+silver-times@users.noreply.github.com>
2023-07-17 21:40:07 +0200

    Add Logout functionality

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import type { PageData } from './$types';
+
+	let user;
+	export let data: PageData;
+	$: console.log(data.user);
 </script>
 
+{!!user}
 <div class="flex flex-col min-h-screen">
 	<div class="navbar bg-[#4A55A2] text-primary-content mx-auto px-5">
 		<div class="flex-1 gap-1">
@@ -12,9 +18,10 @@
 		</div>
 		<div class="flex-none">
 			<ul class="flex gap-5 px-1">
-				<li><a href="/profile" class="text-md uppercase hover:text-gray-200">Profile</a></li>
 				<li><a href="/register" class="text-md uppercase hover:text-gray-200">Register</a></li>
 				<li><a href="/login" class="text-md uppercase hover:text-gray-200">Login</a></li>
+
+				<li><a href="/profile" class="text-md uppercase hover:text-gray-200">Profile</a></li>
 				<li><a href="/logout" class="text-md uppercase hover:text-gray-200">Logout</a></li>
 			</ul>
 		</div>
